0676827df104d3ee041168a70e423b0d309b3343
Felipe Plech <fcplech@gmail.com>
2020-11-29 13:32:35 -0300

    first_class_imp

---
+++ include/ImageProcessor.h
@@ -0,0 +1,38 @@
+#ifndef IMAGEPROCESSOR_H
+#define IMAGEPROCESSO_H
+
+#include <string.h>
+#include "opencv2/opencv.hpp"
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+
+#define IMAGE_DIRECTORY_PATH "../images/linha_pespec.jpg"
+
+#define LOWER_YELLOW_1 10
+#define LOWER_YELLOW_2 50
+#define LOWER_YELLOW_3 0
+
+#define UPPER_YELLOW_1 50
+#define UPPER_YELLOW_2 255
+#define UPPER_YELLOW_3 255
+
+using namespace cv;
+using namespace std;
+
+class ImageProcessor
+{
+public:
+  ImageProcessor();
+  ~ImageProcessor();
+  Mat original_img, warped_img, mask_img, result_img;
+  int width, height;
+  float path_angle;
+  Point2f path_center;
+
+  void getWarpedImage(int offset_x, int offset_y, int offset_x_dst);
+  void getMask();
+  void getResultImage();
+  void printAll();
+};
+
+#endif
